cefef08baeba1d8144085a4bfc749fe49919e93c
lmaoukraine <lmaoukraine1974@gmail.com>
2022-10-19 17:07:04 -0700

    server files

---
+++ start_server.bat
@@ -0,0 +1,3 @@
+java -Xmx20000M -Xms20000M -jar server.jar nogui
+curl -X POST --data "{\"content\": \"^<@^&1013469824141230122^> SERVIDOR MINECRAFT VANILLA 1.19.2 **[FECHADO]**\"}" --header "Content-Type:application/json" https://discord.com/api/webhooks/1013140480680272093/pna8lgI_RI_miciveoffHsY1S85M4tFAM31BFtDHLzZm2dx-B3Vh2wNgVif6CGrvgZiq
+exit
